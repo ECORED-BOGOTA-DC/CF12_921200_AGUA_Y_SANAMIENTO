82addc28f97e5c0465de301f63be9ed41828aec5
luisperez9401 <31638158+luisperez9401@users.noreply.github.com>
2023-08-01 10:12:01 -0500

    Update sintesis.vue

--- src/views/sintesis.vue
+++ src/views/sintesis.vue
@@ -7,7 +7,7 @@
     .row.justify-content-center
       .col-lg-10.mb-5
         figure
-          img(src="@/assets/curso/sintesis.svg", alt="alt")
+          img(src="@/assets/curso/sintesis.svg", alt="Muestra mapa estrategias de intervención en salud pública identificando medidas de prevención, metodología de recolección de información, caracterización de comunidades, enfermedades de interés y salud publica y comunicación social.")
       .col-auto
         a.anexo.mb-5(:href="obtenerLink('/downloads/sintesis.pdf')" target="_blank")
           .anexo__icono
